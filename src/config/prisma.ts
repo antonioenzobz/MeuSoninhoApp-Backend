6201de2a9b2773a19796c301c5f132590e7f687b
Antônio Enzo Bezerra / An1Toh <159924839+antonioenzobz@users.noreply.github.com>
2024-08-11 11:54:37 -0300

    Delete src directory

--- src/config/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient({
-  log: ["info", "error", "query", "warn"],
-  errorFormat: "pretty",
-});
-
-export default prisma;
