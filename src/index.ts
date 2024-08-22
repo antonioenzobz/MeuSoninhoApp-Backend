4f78c92df283d65f8d84d33cd4e30b8ce331fc2a
antonioenzobz <antonioenzobezerra789@gmail.com>
2024-08-22 10:53:20 -0300

    tela de lgin e jwt finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import {cors} from 'hono/cors'
 import usersRoute from './routes/users'
+import loginRoute from './routes/login'
 
 const app = new Hono()
 
@@ -11,5 +12,6 @@ app.get('/', (c) => {
 })
 
 app.route('/users', usersRoute)
+app.route('/login', loginRoute)
 
 export default app
---
+++ src/routes/login.ts
@@ -0,0 +1,33 @@
+import { Hono, Context } from "hono";
+import prisma from "../config/prisma";
+import { getToken } from "../service/token";
+
+const loginRoute = new Hono();
+
+loginRoute.post("/", async (c: Context) => {
+  const body = await c.req.json();
+  const { email, password } = body;
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (!user) {
+    return c.json({ message: "Usuário não encontrado" }, 400);
+  }
+  const verify = Bun.password.verifySync(password, user.password);
+  if (!verify) {
+    return c.json({ message: "Senha inválida" }, 400);
+  }
+
+  return c.json(
+    {
+      token: await getToken(user.id),
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    },
+    200
+  );
+});
+
+export default loginRoute;
